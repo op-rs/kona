3886e8943b8f26d3e8b551d85f7affa54dcf8c21
varun-doshi <doshivarun202@gmail.com>
2025-03-07 02:46:28 +0530

    add docs for RPC Config

--- bin/node/src/cli/config.rs
+++ bin/node/src/cli/config.rs
@@ -1,11 +1,18 @@
+//! RPC Config for the CLI.
+
+/// The RPC Config.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RPCConfig {
+    /// The address to listen on.
     pub listen_addr: String,
+    /// The port to listen on.
     pub listen_port: String,
+    /// Should admin be anabled.
     pub enable_admin: bool,
 }
 
 impl RPCConfig {
+    /// Returns the http endpoint associated with given `RPCConfig`.
     pub fn http_endpoint(&self) -> String {
         format!("http://{}/{}", self.listen_addr, self.listen_port)
     }
