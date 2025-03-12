e8a57de8b3e43b3fff152f4039e889e9170d65a3
Solar Mithril <solarmithril@pm.me>
2025-03-12 14:39:35 +0700

    First stub

---
+++ crates/protocol/interop/src/access_list.rs
@@ -0,0 +1,27 @@
+use crate::CROSS_L2_INBOX_ADDRESS;
+use alloc::vec::Vec;
+use alloy_eips::eip2930::AccessListItem;
+use alloy_primitives::B256;
+
+/// Parses [`AccessListItem`]s to inbox entries.
+///
+/// See [`parse_access_list_item_to_inbox_entries`] for more details. Return flattened iterator with
+/// all inbox entries.
+pub fn parse_access_list_items_to_inbox_entries<'a>(
+    access_list_items: impl Iterator<Item = &'a AccessListItem>,
+) -> impl Iterator<Item = B256> {
+    access_list_items.filter_map(parse_access_list_item_to_inbox_entries).flatten()
+}
+
+/// Parse [`AccessListItem`] to inbox entries, if any.
+// TODO: add url to spec once  [pr](https://github.com/ethereum-optimism/specs/pull/612) is merged
+/// Max 3 inbox entries can exist per [`AccessListItem`] that points to [`CROSS_L2_INBOX_ADDRESS`].
+///
+/// Returns `Vec::new()` if [`AccessListItem`] address doesn't point to [`CROSS_L2_INBOX_ADDRESS`].
+pub fn parse_access_list_item_to_inbox_entries(
+    access_list_item: &AccessListItem,
+) -> Option<Vec<B256>> {
+    (access_list_item.address == CROSS_L2_INBOX_ADDRESS)
+        // TODO: possible to remove clone and return a reference
+        .then(|| access_list_item.storage_keys.clone())
+}
