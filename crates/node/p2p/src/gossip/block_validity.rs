ee9e194f27d7cfd4cd65c607547120b65557ee8c
refcell <abigger87@gmail.com>
2025-04-17 08:49:09 -0400

    demo: try into block

--- crates/node/p2p/src/gossip/block_validity.rs
+++ crates/node/p2p/src/gossip/block_validity.rs
@@ -27,6 +27,9 @@ pub enum BlockInvalidError {
     #[error(transparent)]
     InvalidBlock(#[from] OpPayloadError),
     // TODO: add the rest of the errors variants in follow-up PRs
+    /// Invalid block.
+    #[error(transparent)]
+    InvalidBlock(#[from] OpPayloadError),
 }
 
 impl From<BlockInvalidError> for MessageAcceptance {
