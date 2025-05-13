f0e49c705e0cf26d3c6ed765ae65bbff57eb14d9
theochap <theodore.chap@gmail.com>
2025-05-13 23:29:42 +0000

    fix(node/engine): fix initialize unknowns params

--- crates/node/engine/src/task_queue/tasks/unknowns.rs
+++ crates/node/engine/src/task_queue/tasks/unknowns.rs
@@ -2,22 +2,27 @@
 
 use crate::{EngineClient, EngineState};
 use alloy_eips::eip1898::BlockNumberOrTag;
+use kona_genesis::RollupConfig;
 use std::sync::Arc;
 
 /// Initialize Unknown Engine State.
 ///
 /// For each unknown head, attempt to fetch it from the [`EngineClient`].
-pub async fn init_unknowns(state: &mut EngineState, client: Arc<EngineClient>) {
+pub async fn init_unknowns(
+    state: &mut EngineState,
+    client: Arc<EngineClient>,
+    cfg: &Arc<RollupConfig>,
+) {
     // Initialize the unsafe head if it is not already set.
     if state.unsafe_head.block_info.hash.is_zero() {
         let head = match client.l2_block_info_by_label(BlockNumberOrTag::Pending).await {
             Ok(Some(h)) => h,
             Ok(None) => {
-                warn!(target: "engine", "No pending head found");
+                warn!(target: "engine", "No pending head found.");
                 return;
             }
             Err(e) => {
-                warn!(target: "engine", ?e, "Error fetching pending head");
+                warn!(target: "engine", ?e, "Error fetching pending head.");
                 return;
             }
         };
@@ -29,8 +34,17 @@ pub async fn init_unknowns(state: &mut EngineState, client: Arc<EngineClient>) {
         let head = match client.l2_block_info_by_label(BlockNumberOrTag::Finalized).await {
             Ok(Some(h)) => h,
             Ok(None) => {
-                warn!(target: "engine", "No finalized head found");
-                return;
+                debug!(target: "engine", "No finalized head found. Trying to use genesis.");
+
+                let Ok(Some(head)) = client
+                    .l2_block_info_by_label(BlockNumberOrTag::Number(cfg.genesis.l2.number))
+                    .await
+                else {
+                    warn!(target: "engine", "Impossible to fetch genesis head");
+                    return;
+                };
+
+                head
             }
             Err(e) => {
                 warn!(target: "engine", ?e, "Error fetching finalized head");
@@ -45,8 +59,8 @@ pub async fn init_unknowns(state: &mut EngineState, client: Arc<EngineClient>) {
         let head = match client.l2_block_info_by_label(BlockNumberOrTag::Safe).await {
             Ok(Some(h)) => h,
             Ok(None) => {
-                warn!(target: "engine", "No safe head found");
-                return;
+                debug!(target: "engine", "No safe head found. Using the finalized head");
+                state.finalized_head()
             }
             Err(e) => {
                 warn!(target: "engine", ?e, "Error fetching safe head");
