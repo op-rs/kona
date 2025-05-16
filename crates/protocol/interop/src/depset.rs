8eb84106845b9b1e814b4eb0b7e790b738c52edb
Arun Dhyani <dhyaniarun7@gmail.com>
2025-05-16 18:00:35 +0530

    doc fixes

--- crates/protocol/interop/src/depset.rs
+++ crates/protocol/interop/src/depset.rs
@@ -7,18 +7,17 @@ use kona_registry::HashMap;
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct ChainDependency {
-    /// chain_index is the unique short identifier for this chain
+    /// The unique short identifier for this chain.
     pub chain_index: u32,
 
-    /// activation_time is the timestamp (in seconds) when the chain becomes part of the dependency
-    /// set. This is the minimum timestamp of the inclusion of an executing message that can be
+    /// Timestamp (in seconds) when the chain becomes part of the dependency set. 
+    /// This is the minimum timestamp of the inclusion of an executing message that can be
     /// considered valid.
     pub activation_time: u64,
 
-    /// history_min_time is the lower bound timestamp (in seconds) for data storage and
-    /// accessibility. This is the minimum timestamp of an initiating message that is required
-    /// to be accessible to other chains. This is set to 0 when all data since genesis is
-    /// executable.
+    /// Lower bound timestamp (in seconds) for data storage and accessibility. 
+    /// This is the minimum timestamp of an initiating message that is required to be 
+    /// accessible to other chains. This is set to 0 when all data since genesis is executable.
     pub history_min_time: u64,
 }
 
@@ -27,7 +26,7 @@ pub struct ChainDependency {
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[cfg_attr(feature = "serde", serde(rename_all = "camelCase"))]
 pub struct DependencySet {
-    /// dependencies information per chain
+    /// Dependencies information per chain.
     pub dependencies: HashMap<ChainId, ChainDependency>,
 
     /// Override message expiry window to use for this dependency set.
