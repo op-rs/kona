ee92bb65f59a893252d3646aa3a39cf29c4d912a
Arun Dhyani <dhyaniarun7@gmail.com>
2025-05-16 11:31:42 +0530

    depset methods added

--- crates/protocol/interop/src/depset.rs
+++ crates/protocol/interop/src/depset.rs
@@ -1,5 +1,6 @@
 use alloy_primitives::ChainId;
 use kona_registry::HashMap;
+use crate::MESSAGE_EXPIRY_WINDOW;
 
 /// Configuration for a dependency of a chain
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -31,4 +32,34 @@ pub struct DependencySet {
 
     /// Override message expiry window to use for this dependency set.
     pub override_message_expiry_window: u64,
+}
+
+impl DependencySet {
+  /// Checks if a message is eligible for execution at the given timestamp.
+  ///
+  /// Returns an error if the system cannot currently determine eligibility,
+  /// for example, while the `DependencySet` is still syncing.
+  pub fn can_execute_at(&self, chain_id: ChainId, exec_timestamp: u64) -> bool {
+    self.dependencies.get(&chain_id).map_or(false, |dependency| {
+      exec_timestamp >= dependency.activation_time
+    })
+  }
+
+  /// Determines whether an initiating message can be pulled in at the given timestamp.
+  ///
+  /// Returns an error if the message from the specified chain is not currently readable,
+  /// for example, if the `DependencySet` is still syncing new data.
+  pub fn can_initiate_at(&self, chain_id: ChainId, init_timestamp: u64) -> bool {
+    self.dependencies.get(&chain_id).map_or(false, |dependency| {
+      init_timestamp >= dependency.history_min_time
+    })
+  }
+
+  /// Returns the message expiry window associated with this dependency set.
+  pub fn get_message_expiry_window(&self) -> u64 {
+    if self.override_message_expiry_window == 0 {
+      return MESSAGE_EXPIRY_WINDOW;
+    }
+    return self.override_message_expiry_window;
+  }
 }
