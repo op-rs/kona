e235efdb65db90d5cd7f661ee827a00107ffaf3b
theochap <theodore.chap@gmail.com>
2025-05-20 03:52:41 +0000

    fix(node/kurtosis-e2e-test): fixes the versionning for the e2e testing package

--- tests/node/node_test.go
+++ tests/node/node_test.go
@@ -1,24 +1,53 @@
 package node
 
 import (
+	"encoding/json"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/devnet-sdk/system"
 	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/systest"
 	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/testlib/validators"
+	"github.com/ethereum-optimism/optimism/op-service/apis"
 )
 
-func nodeUpScenario() systest.SystemTestFunc {
+// Really simple system test that just checks that each node has at least 1 peer that is connected to its topics.
+func peerCount() systest.SystemTestFunc {
 	return func(t systest.T, sys system.System) {
 		l2s := sys.L2s()
 
 		for _, l2 := range l2s {
-			config, err := l2.Config()
-			if err != nil {
-				t.Log(err)
-			}
+			for _, node := range l2.Nodes() {
+				clRPC := node.CLRPC()
+				clName := node.CLName()
 
-			t.Log(l2.ID(), l2.Nodes(), config)
+				// 1. Build the payload.
+				reqBody := rpcRequest{
+					JSONRPC: "2.0",
+					Method:  "opp2p_peerStats", // example: Ethereum JSON‑RPC
+					Params:  []any{},
+					ID:      1,
+				}
+
+				rpcResp, err := reqBody.SendRPCRequest(clRPC)
+
+				if err != nil {
+					t.Errorf("failed to send RPC request to node %s: %s", clName, err)
+				} else if rpcResp.Error != nil {
+					t.Errorf("received RPC error from node %s: %s", clName, rpcResp.Error)
+				}
+
+				peerStats := apis.PeerStats{}
+
+				err = json.Unmarshal(rpcResp.Result, &peerStats)
+
+				if err != nil {
+					t.Errorf("failed to unmarshal result: %s", err)
+				}
+
+				if peerStats.Connected < 1 {
+					t.Errorf("node %s has no peers", clName)
+				}
+			}
 		}
 	}
 }
@@ -27,10 +56,10 @@ func TestSystemKonaTest(t *testing.T) {
 	// Get the L2 chain we want to test with
 	chainIdx := uint64(0) // First L2 chain
 
-	nodeValidator := validators.HasSufficientL2Nodes(chainIdx, 1)
+	nodeValidator := validators.HasSufficientL2Nodes(chainIdx, 2)
 
 	systest.SystemTest(t,
-		nodeUpScenario(),
+		peerCount(),
 		nodeValidator,
 	)
 }
