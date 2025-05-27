42efd4ae416342dac042122353242a0f25f68514
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-05-27 14:56:51 +0200

    Rm out of scope comment

--- tests/supervisor/mod.go
+++ tests/supervisor/mod.go
@@ -1,17 +1,3 @@
 package supervisor
 
-/*import (
-	"bytes"
-	"context"
-	"encoding/json"
-	"io"
-	"net/http"
-	"time"
-
-	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/systest"
-	"github.com/ethereum-optimism/optimism/op-service/eth"
-	"github.com/gorilla/websocket"
-	"github.com/stretchr/testify/require"
-)*/
-
 // todo: add tests
