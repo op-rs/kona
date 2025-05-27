bbcb06f66f16cb909672517ac6fd9a2570b6698f
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-05-27 09:19:47 +0200

    Add placeholder for supervisor devnet-sdk tests

---
+++ tests/supervisor/mod.go
@@ -0,0 +1,17 @@
+package supervisor
+
+/*import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/ethereum-optimism/optimism/devnet-sdk/testing/systest"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/gorilla/websocket"
+	"github.com/stretchr/testify/require"
+)*/
+
+// todo: add tests
