78f822183537bdaf620704fc38182517f4ca25cb
varun-doshi <doshivarun202@gmail.com>
2025-05-30 19:59:01 +0530

    fix: apply suggestions

--- crates/node/service/src/service/validator.rs
+++ crates/node/service/src/service/validator.rs
@@ -177,9 +177,9 @@ pub trait ValidatorNodeService {
         let rollup_rpc = RollupRpc::new(engine_query_sender.clone(), l1_watcher_queries_sender);
         launcher = launcher.merge(Some(rollup_rpc.into_rpc())).map_err(Self::Error::from)?;
         if self.config().ws_enabled {
-            let subscriptions_rpc = WsRPC::new(engine_query_sender);
-            launcher =
-                launcher.merge(Some(subscriptions_rpc.into_rpc())).map_err(Self::Error::from)?;
+            launcher = launcher
+                .merge(Some(WsRPC::new(engine_query_sender).into_rpc()))
+                .map_err(Self::Error::from)?;
         }
 
         let handle = launcher.launch().await?;
