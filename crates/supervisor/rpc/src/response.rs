757d868fca6d6af13ded34eb5886052357656c80
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-06-03 13:07:38 +0200

    Add serialization test for SupervisorChainSyncStatus

--- crates/supervisor/rpc/src/response.rs
+++ crates/supervisor/rpc/src/response.rs
@@ -8,7 +8,7 @@ use kona_supervisor_types::SuperHead;
 /// Describes superchain sync status.
 ///
 /// Specs: <https://github.com/ethereum-optimism/specs/blob/main/specs/interop/supervisor.md#supervisorsyncstatus>.
-#[derive(Debug, Default, Clone)]
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
 #[cfg_attr(
     feature = "serde",
     derive(serde::Serialize, serde::Deserialize),
@@ -28,7 +28,7 @@ pub struct SupervisorSyncStatus {
 /// Describes the sync status for a specific chain.
 ///
 /// Specs: <https://github.com/ethereum-optimism/specs/blob/main/specs/interop/supervisor.md#supervisorchainsyncstatus>
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 #[cfg_attr(
     feature = "serde",
     derive(serde::Serialize, serde::Deserialize),
@@ -68,5 +68,73 @@ impl From<SuperHead> for SupervisorChainSyncStatus {
             safe: BlockNumHash::new(safe.number, safe.hash),
             finalized: BlockNumHash::new(finalized.number, finalized.hash),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use alloy_primitives::b256;
+
+    #[cfg(feature = "serde")]
+    #[test]
+    fn test_serialize_supervisor_chain_sync_status() {
+        const STATUS: &'static str = r#"
+            {
+                "unsafe": {
+                    "number": 100,
+                    "hash": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef",
+                    "timestamp": 40044440000,
+                    "parentHash": "0x111def1234567890abcdef1234567890abcdef1234500000abcdef123456aaaa"
+                },
+                "crossUnsafe": {
+                    "number": 90,
+                    "hash": "0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890"
+                },
+                "localSafe": {
+                    "number": 80,
+                    "hash": "0x34567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef13"
+                },
+                "safe": {
+                    "number": 70,
+                    "hash": "0x567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234"
+                },
+                "finalized": {
+                    "number": 60,
+                    "hash": "0x34567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef12"
+                }
+            }"#;
+
+        assert_eq!(
+            serde_json::from_str::<SupervisorChainSyncStatus>(STATUS).expect("should deserialize"),
+            SupervisorChainSyncStatus {
+                r#unsafe: BlockInfo {
+                    number: 100,
+                    hash: b256!(
+                        "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+                    ),
+                    timestamp: 40044440000,
+                    parent_hash: b256!(
+                        "0x111def1234567890abcdef1234567890abcdef1234500000abcdef123456aaaa"
+                    ),
+                },
+                cross_unsafe: BlockNumHash::new(
+                    90,
+                    b256!("0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890")
+                ),
+                local_safe: BlockNumHash::new(
+                    80,
+                    b256!("0x34567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef13")
+                ),
+                safe: BlockNumHash::new(
+                    70,
+                    b256!("0x567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234")
+                ),
+                finalized: BlockNumHash::new(
+                    60,
+                    b256!("0x34567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef12")
+                ),
+            }
+        )
     }
 }
