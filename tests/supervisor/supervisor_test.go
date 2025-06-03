5276e6533bfdf5ca7f19f28d6264b3fcd0ca8dbf
Arun Dhyani <dhyaniarun7@gmail.com>
2025-06-03 11:52:09 +0530

    removed unnecessary test

--- tests/supervisor/supervisor_test.go
+++ tests/supervisor/supervisor_test.go
@@ -18,25 +18,3 @@ func TestInteropSystemSupervisor(t *testing.T) {
 		require.NoError(t, err)
 	})
 }
-
-// func TestInitExecMsg(gt *testing.T) {
-// 	t := devtest.SerialT(gt)
-// 	sys := presets.NewSimpleInterop(t)
-
-// 	t.Require().NotEqual(sys.L2ChainA.ChainID(), sys.L2ChainB.ChainID(), "sanity-check we have two different chains")
-// 	t.Logf("L2 Chain A: %s, L2 Chain B: %s", sys.L2ChainA.ChainID(), sys.L2ChainB.ChainID())
-
-// 	// rng := rand.New(rand.NewSource(1234))
-// 	// alice := sys.FunderA.NewFundedEOA(eth.OneEther)
-// 	// bob := sys.FunderB.NewFundedEOA(eth.OneEther)
-
-// 	// t.Logf("alice: %s, bob: %s", alice.Address(), bob.Address())
-
-// 	// eventLoggerAddress := alice.DeployEventLogger()
-// 	// Trigger random init message at chain A
-// 	// _, _ = alice.SendInitMessage(interop.RandomInitTrigger(rng, eventLoggerAddress, rng.Intn(5), rng.Intn(30)))
-// 	// Make sure supervisor indexes block which includes init message
-// 	// sys.Supervisor.AdvancedUnsafeHead(alice.ChainID(), 2)
-// 	// // Single event in tx so index is 0
-// 	// bob.SendExecMessage(initIntent, 0)
-// }
