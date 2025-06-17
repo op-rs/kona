ae79eb68db830636499d7c03ce26413a9f332085
refcell <abigger87@gmail.com>
2025-06-17 08:52:19 -0400

    Update bin/node/src/flags/supervisor.rs

--- bin/node/src/flags/supervisor.rs
+++ bin/node/src/flags/supervisor.rs
@@ -1,8 +1,4 @@
 //! Supervisor RPC CLI Flags
-//!
-//! These are based on supervisor flags from the [`op-node`][op-node] CLI.
-//!
-//! [op-node]: https://github.com/ethereum-optimism/optimism/blob/develop/op-node/flags/flags.go#L233-L265
 
 use std::{fs, net::IpAddr, path::PathBuf};
 
