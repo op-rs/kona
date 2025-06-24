205816473f0fc0e9e0384ebe4b16e741f8d478da
Arun Dhyani <dhyaniarun7@gmail.com>
2025-06-24 19:41:43 +0530

    review fixes

--- tests/supervisor/config_test.go
+++ tests/supervisor/config_test.go
@@ -7,12 +7,15 @@ import (
 	"github.com/ethereum-optimism/optimism/op-devstack/presets"
 )
 
+const (
+	L2A_CHAIN_ID = "2151908"
+	L2B_CHAIN_ID = "2151909"
+)
+
 func TestNetworkConfig(gt *testing.T) {
 	t := devtest.ParallelT(gt)
 
 	out := presets.NewSimpleInterop(t)
-	t.Require().Equal(out.L2CLA.ChainID().String(), "2151908")
-	t.Require().Equal(out.L2CLB.ChainID().String(), "2151909")
-	t.Require().Equal(out.L2CLA.Peers().TotalConnected, uint(0))
-	t.Require().Equal(out.L2CLB.Peers().TotalConnected, uint(0))
+	t.Require().Equal(out.L2CLA.ChainID().String(), L2A_CHAIN_ID)
+	t.Require().Equal(out.L2CLB.ChainID().String(), L2B_CHAIN_ID)
 }
