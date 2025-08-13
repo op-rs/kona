6496612c584b2361d93753ebb8b81cf9384b7fd5
refcell <abigger87@gmail.com>
2025-08-13 11:31:31 -0400

    fixes

--- docs/sidebar.ts
+++ docs/sidebar.ts
@@ -185,7 +185,7 @@ export const sidebar: SidebarItem[] = [
           { text: "Create a new L1BlockInfoTx Hardfork Variant", link: "/sdk/examples/new-l1-block-info-tx-hardfork" },
           { text: "Create a new kona-executor test fixture", link: "/sdk/examples/executor-test-fixtures" },
           { text: "Configuring P2P Network Peer Scoring", link: "/sdk/examples/p2p-peer-scoring" },
-          { text: "Custom Derivation Pipeline with New Stage", link: "/sdk/examples/custom-derivation-pipeline" }
+          { text: "Custom Derivation Pipeline Stage", link: "/sdk/examples/custom-derivation-pipeline" }
         ]
       }
     ]
