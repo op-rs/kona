546f629364181059958a74216f1c059a607b2352
theochap <theodore.chap@gmail.com>
2025-08-25 10:15:20 -0400

    fix(node/test): fix sync test

--- tests/node/common/sync_test.go
+++ tests/node/common/sync_test.go
@@ -1,7 +1,6 @@
 package node
 
 import (
-	"strings"
 	"sync"
 	"testing"
 
@@ -17,13 +16,14 @@ func TestL2SafeSync(gt *testing.T) {
 
 	out := node_utils.NewMixedOpKona(t)
 
-	nodes := out.L2CLNodes()
+	sequencer := out.L2CLSequencerNodes()[0]
+	nodes := out.L2CLValidatorNodes()
 
 	checkFuns := make([]dsl.CheckFunc, 0, 2*len(nodes))
 
 	for _, node := range nodes {
 		checkFuns = append(checkFuns, node.ReachedFn(types.LocalSafe, 20, 40))
-		checkFuns = append(checkFuns, node.MatchedFn(&nodes[0], types.LocalSafe, 40))
+		checkFuns = append(checkFuns, node.MatchedFn(&sequencer, types.LocalSafe, 40))
 	}
 
 	dsl.CheckAll(t, checkFuns...)
@@ -35,13 +35,14 @@ func TestL2UnsafeSync(gt *testing.T) {
 
 	out := node_utils.NewMixedOpKona(t)
 
-	nodes := out.L2CLNodes()
+	sequencer := out.L2CLSequencerNodes()[0]
+	nodes := out.L2CLValidatorNodes()
 
 	checkFuns := make([]dsl.CheckFunc, 0, 2*len(nodes))
 
 	for _, node := range nodes {
 		checkFuns = append(checkFuns, node.ReachedFn(types.LocalUnsafe, 40, 40))
-		checkFuns = append(checkFuns, node.MatchedFn(&nodes[0], types.LocalUnsafe, 40))
+		checkFuns = append(checkFuns, node.LaggedFn(&sequencer, types.LocalUnsafe, 40, true))
 	}
 
 	dsl.CheckAll(t, checkFuns...)
@@ -65,20 +66,6 @@ func TestL2FinalizedSync(gt *testing.T) {
 	dsl.CheckAll(t, checkFuns...)
 }
 
-func isSequencer(node *dsl.L2CLNode) bool {
-	return strings.Contains(node.Escape().ID().Key(), string(node_utils.Sequencer))
-}
-
-func filterSequencer(nodes []dsl.L2CLNode) []dsl.L2CLNode {
-	out := make([]dsl.L2CLNode, 0, len(nodes))
-	for _, node := range nodes {
-		if isSequencer(&node) {
-			out = append(out, node)
-		}
-	}
-	return out
-}
-
 func TestSyncWithSequencer(gt *testing.T) {
 	t := devtest.ParallelT(gt)
 
@@ -87,9 +74,7 @@ func TestSyncWithSequencer(gt *testing.T) {
 	nodes := out.L2CLValidatorNodes()
 
 	// Find the sequencer nodes.
-	sequencers := filterSequencer(nodes)
-	t.Gate().Equal(len(sequencers), 1, "expected exactly one sequencer")
-	sequencer := sequencers[0]
+	sequencer := out.L2CLSequencerNodes()[0]
 
 	// Check that all the nodes are lagging behind the sequencer for the local unsafe head.
 	var wg sync.WaitGroup
