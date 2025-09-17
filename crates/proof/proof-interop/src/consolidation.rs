bfd16b046e9748d6d9092cb39fb4d1d281d23387
sadiq1971 <sadiqurr8@gmail.com>
2025-09-17 06:35:06 +0600

    removed expect

--- crates/proof/proof-interop/src/consolidation.rs
+++ crates/proof/proof-interop/src/consolidation.rs
@@ -201,17 +201,16 @@ where
                         .and_then(|s| s.try_into().ok())
                         .expect("slice conversion cannot fail")
                 }),
-                min_base_fee: rollup_config.is_jovian_active(header.timestamp).then(|| {
-                    // SAFETY: After the Holocene hardfork, blocks must have the EIP-1559 parameters
-                    // of the chain placed within the header's `extra_data`
-                    // field. This slice index + conversion cannot fail
-                    // unless the protocol rules have been violated.
-                    header
-                        .extra_data
-                        .get(9..17)
-                        .and_then(|s| TryInto::<[u8; 8]>::try_into(s).ok().map(u64::from_be_bytes))
-                        .expect("slice conversion cannot fail")
-                }),
+                min_base_fee: rollup_config
+                    .is_jovian_active(header.timestamp)
+                    .then(|| {
+                        header
+                            .extra_data
+                            .get(9..17)
+                            .and_then(|s| <[u8; 8]>::try_from(s).ok())
+                            .map(u64::from_be_bytes)
+                    })
+                    .flatten(),
             };
 
             // Create a new stateless L2 block executor for the current chain.
