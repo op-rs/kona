5291c573d456ada905a723f8fb40ee7b3264cad8
Ulaş <ilikecodewithmyhand@gmail.com>
2025-09-25 01:00:25 +0800

    chore(node): fix missing semicolons in return statements

--- crates/node/engine/src/attributes.rs
+++ crates/node/engine/src/attributes.rs
@@ -159,7 +159,7 @@ impl AttributesMatch {
             if &attr_tx != block_tx.inner.inner.inner() {
                 warn!(target: "engine", ?attr_tx, ?block_tx, "Transaction mismatch in derived attributes");
                 return AttributesMismatch::TransactionContent(attr_tx.tx_hash(), block_tx.tx_hash())
-                    .into()
+                    .into();
             }
         }
 
@@ -244,7 +244,7 @@ impl AttributesMatch {
                 BaseFeeParams { max_change_denominator: ad, elasticity_multiplier: ae },
                 BaseFeeParams { max_change_denominator: bd, elasticity_multiplier: be },
             )
-            .into()
+            .into();
         }
 
         Self::Match
@@ -290,7 +290,7 @@ impl AttributesMatch {
 
         // Check transactions
         if let mismatch @ Self::Mismatch(_) = Self::check_transactions(attributes_txs, block) {
-            return mismatch
+            return mismatch;
         }
 
         let Some(gas_limit) = attributes.inner().gas_limit else {
