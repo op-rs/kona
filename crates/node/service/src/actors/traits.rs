defa93f7f7955fe46bccee07e3ee77f35a26fefc
op-will <232669456+op-will@users.noreply.github.com>
2025-11-20 22:18:07 -0600

    PR Feedback: Remove outdated NodeActor lifecycle comment

--- crates/node/service/src/actors/traits.rs
+++ crates/node/service/src/actors/traits.rs
@@ -15,15 +15,6 @@ pub trait CancellableContext: Send {
 /// - Handle incoming messages.
 ///     - Perform background tasks.
 /// - Emit new events for other actors to process.
-///
-/// The types and functions that are part of a `NodeActor` imply a specific lifecycle:
-/// 1. Build: Create the Actor, passing in configuration that is known before other actors are
-///    created. Actors cannot have any dependencies on each other at `build` time.
-/// 2. Init: Create the struct necessary to call `start`. This is after Build because the `start`
-///    logic for one actor may depend on data structures that were created by other actors when
-///    their `build` function was called. These dependencies are bundled into `StartData` during
-///    `init`.
-/// 3. Start: The entrypoint into the long-running actor logic.
 #[async_trait]
 pub trait NodeActor: Send + 'static {
     /// The error type for the actor.
