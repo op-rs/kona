6c463fed2f228c1a50c71c1c5af1a53997511774
Justin <czekotilodanilo@gmail.com>
2025-11-27 00:14:16 +0100

    Update client.rs

--- crates/node/engine/src/client.rs
+++ crates/node/engine/src/client.rs
@@ -287,12 +287,8 @@ impl OpEngineApi<Optimism, Http<HyperAuthClient>> for EngineClient {
         payload: OpExecutionPayloadV4,
         parent_beacon_block_root: B256,
     ) -> TransportResult<PayloadStatus> {
-        let call = self.rollup_boost.server.new_payload_v4(
-            payload.clone(),
-            vec![],
-            parent_beacon_block_root,
-            vec![],
-        );
+        let call =
+            self.rollup_boost.server.new_payload_v4(payload, vec![], parent_beacon_block_root, vec![]);
 
         record_call_time(call, Metrics::NEW_PAYLOAD_METHOD).await.map_err(Into::into)
     }
