52bf518a88b9636172272c4a5d8064ff03d4439e
Einar Rasmussen <einar@oplabs.co>
2025-12-02 14:38:16 +0100

    _

--- docker/recipes/kona-node-dev/generate-jwt.sh
+++ docker/recipes/kona-node-dev/generate-jwt.sh
@@ -7,6 +7,7 @@ mkdir -p "${SCRIPT_DIR}/jwttoken"
 if [[ ! -f "${SCRIPT_DIR}/jwttoken/jwt.hex" ]]
 then
   openssl rand -hex 32 | tr -d "\n" | tee > "${SCRIPT_DIR}/jwttoken/jwt.hex"
+  echo "Generated a JWT secret at ${SCRIPT_DIR}/jwttoken/jwt.hex"
 else
   echo "${SCRIPT_DIR}/jwttoken/jwt.hex already exists!"
 fi
