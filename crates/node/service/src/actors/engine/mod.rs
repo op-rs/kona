3bf4a727247149f506cb9cd45aea9edb11a9a397
op-will <232669456+op-will@users.noreply.github.com>
2025-12-19 09:58:06 -0600

    Moving engine requests into client.rs file

--- crates/node/service/src/actors/engine/mod.rs
+++ crates/node/service/src/actors/engine/mod.rs
@@ -1,16 +1,17 @@
 //! The [`EngineActor`] and its components.
 
 mod actor;
-pub use actor::{
-    BuildRequest, EngineActor, EngineActorRequest, EngineConfig, EngineContext, EngineInboundData,
-    EngineRpcRequest, ResetRequest, SealRequest,
-};
+pub use actor::{EngineActor, EngineConfig, EngineContext, EngineInboundData};
 
 mod client;
-pub use client::{EngineClientError, EngineClientResult};
+pub use client::{
+    BuildRequest, EngineActorRequest, EngineClientError, EngineClientResult, EngineRpcRequest,
+    ResetRequest, SealRequest,
+};
 
 mod error;
 pub use error::EngineError;
 
 mod finalizer;
+
 pub use finalizer::L2Finalizer;
